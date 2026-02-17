bcb91eef137098ea4a0b116db5dea53d0963bed0
Alwayshadzxyz <hadzzokeyy@gmail.com>
2026-02-17 20:31:54 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,14 @@
 let currentEmail = ""
 
-async function generateEmail() {
+function generateEmail() {
+  const random = Math.random().toString(36).substring(2, 10);
+  const email = random + "@hadzzmail.com";
+
+  document.getElementById("emailDisplay").innerText = email;
+
+  // Simpan ke localStorage
+  localStorage.setItem("generatedEmail", email);
+
   const res = await fetch("/api/generate")
   const data = await res.json()
   currentEmail = data.email
