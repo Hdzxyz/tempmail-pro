ce2f66d57a8cbf8764c237d25369d469463e3628
Alwayshadzxyz <hadzzokeyy@gmail.com>
2026-02-17 18:57:20 +0700

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,38 @@
+let currentEmail = ""
+
+async function generateEmail() {
+  const res = await fetch("/api/generate")
+  const data = await res.json()
+  currentEmail = data.email
+  document.getElementById("email").innerText = currentEmail
+}
+
+function copyEmail() {
+  if (!currentEmail) return
+  navigator.clipboard.writeText(currentEmail)
+  alert("Email copied!")
+}
+
+async function loadInbox() {
+  if (!currentEmail) return
+
+  const [login, domain] = currentEmail.split("@")
+  const res = await fetch(`/api/inbox?login=${login}&domain=${domain}`)
+  const data = await res.json()
+
+  let html = ""
+  if (data.length === 0) {
+    html = "No messages"
+  } else {
+    data.forEach(msg => {
+      html += `<div>
+        <b>${msg.from}</b><br>
+        ${msg.subject}
+      </div><hr>`
+    })
+  }
+
+  document.getElementById("inbox").innerHTML = html
+}
+
+setInterval(loadInbox, 5000)
